895e30782ea3372501592ae59af48728c2244352
xiao_hei_hand <1919321049@qq.com>
2026-03-14 21:37:15 +0800

    feat(UIlib) : 添加了UI库，具体日志看旧仓库

---
+++ src/main/java/org/agmas/noellesroles/client/widget/nodes/LabelButtonNodeWidget.java
@@ -0,0 +1,15 @@
+package org.agmas.noellesroles.client.widget.nodes;
+
+import net.minecraft.network.chat.Component;
+
+/**
+ * 带标签的按钮控件
+ * <p>
+ *
+ * </p>
+ */
+public class LabelButtonNodeWidget extends LabelNodeWidget{
+    protected LabelButtonNodeWidget(int x, int y, int w, int h, Component component) {
+        super(x, y, w, h, component);
+    }
+}
