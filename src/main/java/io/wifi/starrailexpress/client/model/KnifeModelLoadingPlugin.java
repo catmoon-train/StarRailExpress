7624166bdeae02dc04e9ec24bc02a235d8be3075
xiao_hei_hand <1919321049@qq.com>
2026-03-15 17:16:09 +0800

    feat(Skin) : 修改Skin从enum改为class，并完成相关调用的修改
     - 将skin存入skinManager的map中
     - 将bakeModels改为map存储

--- src/main/java/io/wifi/starrailexpress/client/model/KnifeModelLoadingPlugin.java
+++ src/main/java/io/wifi/starrailexpress/client/model/KnifeModelLoadingPlugin.java
@@ -2,6 +2,7 @@ package io.wifi.starrailexpress.client.model;
 
 
 import io.wifi.starrailexpress.item.KnifeItem;
+import io.wifi.starrailexpress.util.SkinManager;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
@@ -11,8 +12,8 @@ public class KnifeModelLoadingPlugin implements ModelLoadingPlugin {
 
     public static final ModelResourceLocation KNIFE_MODEL_ID = ModelResourceLocation.inventory(KnifeItem.ITEM_ID);
 
-    public static ResourceLocation getModelLocation(KnifeItem.Skin skin, Variant variant) {
-        var skinPart = skin == KnifeItem.Skin.DEFAULT ? "" : "_%s".formatted(skin.getName());
+    public static ResourceLocation getModelLocation(SkinManager.Skin skin, Variant variant) {
+        var skinPart = skin == SkinManager.DEFAULT_SKIN ? "" : "_%s".formatted(skin.getName());
         var variantPart = variant == Variant.DEFAULT ? "" : "_%s".formatted(variant.getSerializedName());
 
         return KNIFE_MODEL_ID.id().withPath(path -> "item/%s%s%s".formatted(path, skinPart, variantPart));
@@ -21,7 +22,7 @@ public class KnifeModelLoadingPlugin implements ModelLoadingPlugin {
     @Override
     public void onInitializeModelLoader(Context pluginContext) {
         // make sure all models get loaded
-        for (KnifeItem.Skin skin : KnifeItem.Skin.values()) {
+        for (SkinManager.Skin skin : SkinManager.getSkins().values()) {
             for (Variant variant : Variant.values()) {
                 pluginContext.addModels(getModelLocation(skin, variant));
             }
