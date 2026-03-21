d9014d771df7f721cbc6120389cf6425b26013b5
wifi-left <ysy1679682327@outlook.com>
2026-03-21 19:31:09 +0800

    Fix crash on server

--- src/main/java/org/agmas/noellesroles/mixin/roles/stalker/StalkerOffItemMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/roles/stalker/StalkerOffItemMixin.java
@@ -1,30 +1,24 @@
 package org.agmas.noellesroles.mixin.roles.stalker;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.OptionInstance;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.player.Player;
 import org.agmas.noellesroles.init.ModItems;
-import org.agmas.noellesroles.item.StalkerKnifeItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Arrays;
-
 @Mixin(Player.class)
 public abstract class StalkerOffItemMixin {
 
-
     @Inject(method = "getMainArm", at = @At("RETURN"), cancellable = true)
     private void mainHand(CallbackInfoReturnable<HumanoidArm> cir) {
-        if (Minecraft.getInstance().player != null){
-            if (Minecraft.getInstance().player.getMainHandItem().getItem() == ModItems.STALKER_KNIFE_OFFHAND){
-                if (cir.getReturnValue()==HumanoidArm.RIGHT){
+        final Player player = (Player) (Object) this;
+        if (player != null) {
+            if (player.getMainHandItem().getItem() == ModItems.STALKER_KNIFE_OFFHAND) {
+                if (cir.getReturnValue() == HumanoidArm.RIGHT) {
                     cir.setReturnValue(HumanoidArm.LEFT);
-                }
-                else{
+                } else {
                     cir.setReturnValue(HumanoidArm.RIGHT);
                 }
             }
