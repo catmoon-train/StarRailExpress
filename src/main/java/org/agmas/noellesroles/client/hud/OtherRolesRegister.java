5d80b18932634787f0391f76d97b59cb8979545f
wifi-left <ysy1679682327@outlook.com>
2026-03-29 16:36:08 +0800

    柜子区有hud显示了！

--- src/main/java/org/agmas/noellesroles/client/hud/OtherRolesRegister.java
+++ src/main/java/org/agmas/noellesroles/client/hud/OtherRolesRegister.java
@@ -5,27 +5,28 @@ import org.agmas.noellesroles.client.hud.roles.*;
 public class OtherRolesRegister {
 
     public static void registerSons() {
+        AdmirerHud.register();
         AvengerHud.register();
         BomberHud.register();
         BoxerHud.register();
-        DetectiveHud.register();
-        GamblerHud.register();
-        MonitorHud.register();
-        PsychologistHud.register();
-        PuppeteerHud.register();
-        SingerHud.register();
-        SuperStarHud.register();
-        AdmirerHud.register();
         BroadcasterHud.register();
+        DetectiveHud.register();
         DetectivePassiveHud.register();
         DIOHud.register();
+        ExecutionerHud.register();
+        GamblerHud.register();
         InsaneHud.register();
         MagicianHud.register();
+        MonitorHud.register();
         MorphlingHud.register();
         NianShouHud.register();
         PhantomHud.register();
+        PsychologistHud.register();
+        PuppeteerHud.register();
         RecallerHud.register();
         SeaKingHud.register();
+        SingerHud.register();
+        SuperStarHud.register();
         TrapperHud.register();
         VultureHud.register();
         WaterGhostHud.register();
