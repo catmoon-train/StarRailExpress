74af926a20af6bcac7fc0271d7497a0bf1b00fda
wifi-left <ysy1679682327@outlook.com>
2026-04-12 14:03:47 +0800

    调整顺序，愚者启用条件

--- src/main/java/org/agmas/noellesroles/roles/thief/ThiefPlayerComponent.java
+++ src/main/java/org/agmas/noellesroles/roles/thief/ThiefPlayerComponent.java
@@ -798,20 +798,7 @@ public class ThiefPlayerComponent implements RoleComponent, ServerTickingCompone
         var gwc = SREGameWorldComponent.KEY.get(player.level());
         if (!gwc.isRole(player, ModRoles.THIEF))
             return;
-        if (player.hasEffect(ModEffects.NO_COLLIDE)) // 安全时间
-            return;
-        if (this.honorCost < 100)
-            return;
-        var psc = SREPlayerShopComponent.KEY.get(player);
-        if (player.level().getGameTime() % 20 == 0) {
-            if (psc.balance >= this.honorCost) {
-                if (RoleUtils.insertStackInFreeSlot(player, Items.GOLD_INGOT.getDefaultInstance())) {
-                    psc.addToBalance(-honorCost);
-                    player.displayClientMessage(
-                            Component.translatable("message.thief.honor_got").withStyle(ChatFormatting.GOLD), true);
-                }
-            }
-        }
+        
         if (this.cooldown > 0) {
             this.cooldown--;
             if (this.cooldown % 60 == 0 || this.cooldown == 0) {
@@ -819,6 +806,7 @@ public class ThiefPlayerComponent implements RoleComponent, ServerTickingCompone
             }
         }
 
+        
         // 处理延迟通知
         if (!pendingNotifications.isEmpty()) {
             java.util.Iterator<PendingNotification> iterator = pendingNotifications.iterator();
@@ -834,6 +822,21 @@ public class ThiefPlayerComponent implements RoleComponent, ServerTickingCompone
                                 true);
                     }
                     iterator.remove();
+                }
+            }
+        }
+        
+        if (player.hasEffect(ModEffects.NO_COLLIDE)) // 安全时间
+            return;
+        if (this.honorCost < 100)
+            return;
+        var psc = SREPlayerShopComponent.KEY.get(player);
+        if (player.level().getGameTime() % 20 == 0) {
+            if (psc.balance >= this.honorCost) {
+                if (RoleUtils.insertStackInFreeSlot(player, Items.GOLD_INGOT.getDefaultInstance())) {
+                    psc.addToBalance(-honorCost);
+                    player.displayClientMessage(
+                            Component.translatable("message.thief.honor_got").withStyle(ChatFormatting.GOLD), true);
                 }
             }
         }
