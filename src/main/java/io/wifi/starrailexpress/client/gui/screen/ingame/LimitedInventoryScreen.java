213831bfee97f60cbbd999502c6f40a50c0adfa0
wifi-left <ysy1679682327@outlook.com>
2026-04-23 16:09:50 +0800

    多行商店（？）

--- src/main/java/io/wifi/starrailexpress/client/gui/screen/ingame/LimitedInventoryScreen.java
+++ src/main/java/io/wifi/starrailexpress/client/gui/screen/ingame/LimitedInventoryScreen.java
@@ -14,9 +14,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Renderable;
-import net.minecraft.client.gui.components.events.GuiEventListener;
-import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
@@ -31,13 +28,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Environment(EnvType.CLIENT)
-
 public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> {
 
     public static final ResourceLocation BACKGROUND_TEXTURE = SRE
             .watheId("textures/gui/container/limited_inventory.png");
-    // public static final @NotNull ResourceLocation ID =
-    // SRE.watheId("textures/gui/game.png");
+
+    private static final int SHOP_ITEM_SPACING_X = 38;
+    private static final int SHOP_ITEM_SPACING_Y = 44;
+    private int SHOP_MAX_ROWS_PER_PAGE = 1;
+    private static final int SHOP_TOP_SAFE_Y = 20;
+
     public final LocalPlayer player;
 
     public LimitedInventoryScreen(@NotNull LocalPlayer player) {
@@ -51,16 +51,53 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
     public ArrayList<Button> menuSelections = new ArrayList<>();
     public boolean isMenuOpen = false;
 
+    private final ArrayList<StoreItemWidget> shopWidgets = new ArrayList<>();
+    private int shopCurrentPage = 0;
+    private int shopTotalPages = 1;
+    private int shopColumns = 1;
+    private int shopRowsOnCurrentPage = 1;
+
+    private int shopGridStartY = 0;
+    private int shopNavY = 0;
+
+    private Button shopPrevPageButton = null;
+    private Button shopNextPageButton = null;
+
     public void toggleViewMenu(boolean flag) {
         this.isMenuOpen = flag;
-        menuButton.setMessage(
-                Component.translatable("screen.limited_inventory.button.menu." + (!isMenuOpen ? "show" : "hide")));
-        {
-            for (var ms : menuSelections) {
-                ms.visible = this.isMenuOpen;
-                ms.active = this.isMenuOpen;
-            }
+        if (menuButton != null) {
+            menuButton.setMessage(
+                    Component.translatable("screen.limited_inventory.button.menu." + (!isMenuOpen ? "show" : "hide")));
         }
+        for (var ms : menuSelections) {
+            ms.visible = this.isMenuOpen;
+            ms.active = this.isMenuOpen;
+        }
+    }
+
+    public static List<ShopEntry> getRoleShopEntries(SRERole role) {
+        if (role == null)
+            return List.of();
+        final var shopEntries = ShopContent.getShopEntries(role.getIdentifier());
+        if (!shopEntries.isEmpty())
+            return shopEntries;
+        if (role.canUseKiller())
+            return ShopContent.defaultKnifeEntries;
+        return List.of();
+    }
+
+    public List<ShopEntry> getShopEntries() {
+        final var player = Minecraft.getInstance().player;
+        var gameWorldComponent = SREClient.gameComponent;
+        if (gameWorldComponent == null)
+            return List.of();
+        if (SREClient.gameComponent != null && SREClient.isPlayerAliveAndInSurvival()) {
+            final var role = gameWorldComponent.getRole(player);
+            if (role == null)
+                return List.of();
+            return getRoleShopEntries(role);
+        }
+        return List.of();
     }
 
     public static class ShopEntryDisplayItem extends ShopEntry {
@@ -73,8 +110,7 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
             this.index = index;
         }
 
-        public static ArrayList<ShopEntryDisplayItem> transferArrayList(List<ShopEntry> shopEntries,
-                Player player) {
+        public static ArrayList<ShopEntryDisplayItem> transferArrayList(List<ShopEntry> shopEntries, Player player) {
             ArrayList<ShopEntryDisplayItem> displayAbleEntries = new ArrayList<>();
             int idx = 0;
             for (var entry : shopEntries) {
@@ -93,28 +129,42 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
     protected void init() {
         super.init();
         initMenuSelections();
-        List<ShopEntry> entries = (getShopEntries());
+
+        shopWidgets.clear();
+        shopCurrentPage = 0;
+
+        List<ShopEntry> entries = getShopEntries();
         List<ShopEntryDisplayItem> displayAbleEntries = ShopEntryDisplayItem.transferArrayList(entries, player);
-        if (displayAbleEntries.isEmpty())
-            return;
-        int apart = 38;
-        int x = this.width / 2 - displayAbleEntries.size() * apart / 2 + 9;
-        int y = this.y - 46;
-        final var gameComponent = SREClient.gameComponent;
-        if (gameComponent != null) {
-            final var role = gameComponent.getRole(player);
-            if (role.getAddChild() != null) {
-                role.getAddChild().accept(this);
+
+        if (!displayAbleEntries.isEmpty()) {
+            final var gameComponent = SREClient.gameComponent;
+            if (gameComponent != null) {
+                final var role = gameComponent.getRole(player);
+                if (role != null && role.getAddChild() != null) {
+                    role.getAddChild().accept(this);
+                }
+            }
+
+            for (var t : displayAbleEntries) {
+                var widget = new StoreItemWidget(this, 0, 0, t, t.index);
+                shopWidgets.add(widget);
+                this.addRenderableWidget(widget);
             }
         }
-        for (int i = 0; i < displayAbleEntries.size(); i++) {
-            var t = displayAbleEntries.get(i);
-            this.addRenderableWidget(new StoreItemWidget(this, x + apart * i, y, t, t.index));
-        }
+
+        shopPrevPageButton = this.addRenderableWidget(
+                Button.builder(Component.literal("<"), (btn) -> changeShopPage(-1))
+                        .bounds(0, -8, 20, 20)
+                        .build());
+        shopNextPageButton = this.addRenderableWidget(
+                Button.builder(Component.literal(">"), (btn) -> changeShopPage(1))
+                        .bounds(0, -8, 20, 20)
+                        .build());
+
+        refreshShopLayout();
     }
 
     public void initMenuSelections() {
-
         menuButton = org.agmas.noellesroles.client.widget.custom_button.ModernButton
                 .builder(Component.translatable("screen.limited_inventory.button.menu"), (btn) -> {
                     toggleViewMenu(!this.isMenuOpen);
@@ -129,44 +179,92 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
         toggleViewMenu(false);
     }
 
-    public <T extends GuiEventListener & Renderable & NarratableEntry> T addRenderableWidget(T drawableElement) {
-        ((DrawableGet) this).getDrawable().add(drawableElement);
-        return (T) this.addWidget(drawableElement);
+    private void changeShopPage(int delta) {
+        int next = Math.max(0, Math.min(shopTotalPages - 1, shopCurrentPage + delta));
+        if (next != shopCurrentPage) {
+            shopCurrentPage = next;
+            refreshShopLayout();
+        }
     }
 
-    public static List<ShopEntry> getRoleShopEntries(SRERole role) {
-        if (role == null)
-            return List.of();
-        final var shopEntries = ShopContent.getShopEntries(
-                role.getIdentifier());
-        if (!shopEntries.isEmpty()) {
-            return shopEntries;
-        }
-        if (role.canUseKiller()) {
-            return ShopContent.defaultKnifeEntries;
-        }
-        return List.of();
-    }
-
-    public List<ShopEntry> getShopEntries() {
-        final var player = Minecraft.getInstance().player;
-        var gameWorldComponent = SREClient.gameComponent;
-        if (gameWorldComponent == null)
-            return List.of();
-        if (SREClient.gameComponent != null && SREClient.isPlayerAliveAndInSurvival()) {
-            final var role = gameWorldComponent.getRole(player);
-            if (role == null)
-                return List.of();
-            final var shopEntries = ShopContent.getShopEntries(
-                    role.getIdentifier());
-            if (!shopEntries.isEmpty()) {
-                return shopEntries;
+    private void refreshShopLayout() {
+        SHOP_MAX_ROWS_PER_PAGE = Math.max(1, Math.min((this.y - SHOP_TOP_SAFE_Y - 4) / 60, 4));
+        SRE.LOGGER.info("super.y:" + super.y);
+        int count = shopWidgets.size();
+        if (count <= 0) {
+            shopTotalPages = 1;
+            shopRowsOnCurrentPage = 1;
+            if (shopPrevPageButton != null) {
+                shopPrevPageButton.visible = false;
+                shopPrevPageButton.active = false;
             }
+            if (shopNextPageButton != null) {
+                shopNextPageButton.visible = false;
+                shopNextPageButton.active = false;
+            }
+            return;
         }
-        if (gameWorldComponent.canUseKillerFeatures(player)) {
-            return ShopContent.defaultKnifeEntries;
+
+        int availableWidth = Math.max(1, (int) (this.width * 0.8f));
+        shopColumns = Math.max(1, availableWidth / SHOP_ITEM_SPACING_X);
+
+        int totalRows = (count + shopColumns - 1) / shopColumns;
+        shopTotalPages = (totalRows + SHOP_MAX_ROWS_PER_PAGE - 1) / SHOP_MAX_ROWS_PER_PAGE;
+        shopCurrentPage = Math.max(0, Math.min(shopCurrentPage, shopTotalPages - 1));
+
+        int startRow = shopCurrentPage * SHOP_MAX_ROWS_PER_PAGE;
+        int remainingRows = Math.max(0, totalRows - startRow);
+        shopRowsOnCurrentPage = Math.max(1, Math.min(SHOP_MAX_ROWS_PER_PAGE, remainingRows));
+
+        int baseY = this.y - 50;
+        int offsetUp = shopRowsOnCurrentPage > 1 ? 20 : 0;
+        shopGridStartY = Math.max(SHOP_TOP_SAFE_Y, baseY - offsetUp);
+
+        for (int i = 0; i < count; i++) {
+            var widget = shopWidgets.get(i);
+            int globalRow = i / shopColumns;
+            int col = i % shopColumns;
+
+            boolean visible = globalRow >= startRow && globalRow < startRow + shopRowsOnCurrentPage;
+            widget.visible = visible;
+            widget.active = visible;
+
+            if (!visible)
+                continue;
+
+            int localRow = globalRow - startRow;
+            int itemsInThisRow = Math.min(shopColumns, count - globalRow * shopColumns);
+
+            int rowStartX = this.width / 2 - ((itemsInThisRow) * SHOP_ITEM_SPACING_X) / 2 + 10;
+            int x = rowStartX + col * SHOP_ITEM_SPACING_X;
+            int y = shopGridStartY + localRow * SHOP_ITEM_SPACING_Y;
+
+            widget.setPosition(x, y);
         }
-        return List.of();
+
+        shopNavY = Math.min(this.height - 24, shopGridStartY + shopRowsOnCurrentPage * SHOP_ITEM_SPACING_Y - 16);
+
+        boolean needPaging = shopTotalPages > 1;
+        if (shopPrevPageButton != null && shopNextPageButton != null) {
+            if (needPaging) {
+                Component pageText = Component.literal((shopCurrentPage + 1) + " / " + shopTotalPages);
+                int textW = this.font.width(pageText);
+                int gap = 6;
+                int btnW = 20;
+
+                int centerX = this.width / 2;
+                int prevX = centerX - (textW / 2) - gap - btnW;
+                int nextX = centerX + (textW / 2) + gap;
+
+                shopPrevPageButton.setPosition(prevX, shopNavY);
+                shopNextPageButton.setPosition(nextX, shopNavY);
+            }
+
+            shopPrevPageButton.visible = needPaging;
+            shopNextPageButton.visible = needPaging;
+            shopPrevPageButton.active = needPaging && shopCurrentPage > 0;
+            shopNextPageButton.active = needPaging && shopCurrentPage < shopTotalPages - 1;
+        }
     }
 
     @Override
@@ -175,24 +273,28 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
 
         context.pose().pushPose();
         context.pose().translate(context.guiWidth() / 2f, context.guiHeight(), 0);
-        // float scale = 0.28f;
-        // context.pose().scale(scale, scale, 1f);
-        // int height = 254;
-        // int width = 497;
-        // context.pose().translate(0, -230, 0);
-        // int xOffset = 0;
-        // int yOffset = 0;
-        // // context.innerBlit(ID, (int) (xOffset - width / 2f), (int) (xOffset + width
-        // / 2f), (int) (yOffset - height / 2f),
-        // (int) (yOffset + height / 2f), 0, 0, 1f, 0, 1f, 1f, 1f, 1f, 1f);
         context.pose().popPose();
+    }
+
+    @Override
+    public void resize(@NotNull Minecraft minecraft, int width, int height) {
+        super.resize(minecraft, width, height);
+        refreshShopLayout();
     }
 
     @Override
     public void render(GuiGraphics context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
+
+        if (shopTotalPages > 1) {
+            Component pageText = Component.literal((shopCurrentPage + 1) + " / " + shopTotalPages);
+            int textX = this.width / 2 - this.font.width(pageText) / 2;
+            int textY = shopNavY + (20 - this.font.lineHeight) / 2; // 与 20x20 按钮垂直居中
+            context.drawString(this.font, pageText, textX, textY, 0xFFFFFF, false);
+        }
+
         this.drawMouseoverTooltip(context, mouseX, mouseY);
-        StoreRenderer.renderHud(this.font, this.player, (context), delta);
+        StoreRenderer.renderHud(this.font, this.player, context, delta);
     }
 
     public static class StoreItemWidget extends Button {
@@ -211,7 +313,6 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
             super.renderWidget(context, mouseX, mouseY, delta);
             context.blitSprite(entry.type().getTexture(), this.getX() - 7, this.getY() - 7, 30, 30);
 
-            // - 7, 30, 30);
             context.renderItem(this.entry.stack(), this.getX(), this.getY());
             if (this.isHovered()) {
                 this.screen.renderLimitedInventoryTooltip(context, this.entry.stack());
@@ -224,8 +325,6 @@ public class LimitedInventoryScreen extends LimitedHandledScreen<InventoryMenu> 
 
         private void drawShopSlotHighlight(GuiGraphics context, int x, int y, int z) {
             int color = 0x90FFBF49;
-            // context.fillGradient(RenderLayer.getGuiOverlay(), x, y, x + 16, y + 16,
-            // color, color, z);
             context.fillGradient(RenderType.guiOverlay(), x, y, x + 16, y + 14, color, color, z);
             context.fillGradient(RenderType.guiOverlay(), x, y + 14, x + 15, y + 15, color, color, z);
             context.fillGradient(RenderType.guiOverlay(), x, y + 15, x + 14, y + 16, color, color, z);
