2e9098df849a9f0e2513d26b288dd3ac8ca4d0dc
wifi-left <ysy1679682327@outlook.com>
2026-04-25 22:17:42 +0800

    限制死亡原因

--- src/main/java/org/agmas/noellesroles/content/item/RiotShieldHandler.java
+++ src/main/java/org/agmas/noellesroles/content/item/RiotShieldHandler.java
@@ -9,6 +9,7 @@ import net.minecraft.sounds.SoundSource;
 import org.agmas.noellesroles.init.ModItems;
 
 import io.wifi.starrailexpress.event.AllowPlayerDeathWithKiller;
+import io.wifi.starrailexpress.game.GameConstants;
 import io.wifi.starrailexpress.game.GameUtils;
 
 public class RiotShieldHandler {
@@ -18,9 +19,15 @@ public class RiotShieldHandler {
 
     public static boolean allowDeath(Player victim, Player attacker,
             ResourceLocation deathReason) {
-        if (!GameUtils.isPlayerAliveAndSurvivalIgnoreShitSplit(attacker) || !GameUtils.isPlayerAliveAndSurvivalIgnoreShitSplit(victim))
+        if (!GameUtils.isPlayerAliveAndSurvivalIgnoreShitSplit(attacker)
+                || !GameUtils.isPlayerAliveAndSurvivalIgnoreShitSplit(victim))
             return true;
-
+        if (!deathReason.equals(GameConstants.DeathReasons.REVOLVER)
+                && !deathReason.equals(GameConstants.DeathReasons.ARROW)
+                && !deathReason.equals(GameConstants.DeathReasons.DERRINGER)
+                && !!deathReason.equals(GameConstants.DeathReasons.BAT)
+                && !deathReason.equals(GameConstants.DeathReasons.GRENADE))
+            return true;
         // 如果受害者正在举盾且主手/副手持有我们的防暴盾，则阻挡并损坏盾
         if (!victim.isUsingItem())
             return true;
