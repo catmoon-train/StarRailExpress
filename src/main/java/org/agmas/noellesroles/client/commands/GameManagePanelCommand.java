7c56af780aaa8e2465eb800a924136a932a25b6c
wifi-left <ysy1679682327@outlook.com>
2026-04-30 13:30:01 +0800

    客户端设置UI快捷命令

--- src/main/java/org/agmas/noellesroles/client/commands/GameManagePanelCommand.java
+++ src/main/java/org/agmas/noellesroles/client/commands/GameManagePanelCommand.java
@@ -1,5 +1,6 @@
 package org.agmas.noellesroles.client.commands;
 
+import io.wifi.ConfigCompact.ui.SettingMenuScreen;
 import io.wifi.starrailexpress.client.util.ClientScheduler;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
@@ -8,27 +9,35 @@ import net.minecraft.network.chat.Component;
 import org.agmas.noellesroles.client.screen.GameManagementScreen;
 
 public class GameManagePanelCommand {
-    public static void register() {
-        ClientCommandRegistrationCallback.EVENT.register(
-                (dispatcher, registryAccess) -> {
-                    dispatcher.register(ClientCommandManager.literal("sre:client")
-                            .then(ClientCommandManager.literal("screen")
-                                    .then(ClientCommandManager.literal("GameManagePanel")
-                                            .executes(context -> {
-                                                if (context.getSource().getPlayer().hasPermissions(2)) {
-                                                    ClientScheduler.schedule(() -> {
-                                                        context.getSource().getClient()
-                                                                .setScreen(new GameManagementScreen());
-                                                    }, 1);
-                                                } else {
-                                                    context.getSource()
-                                                            .sendError(
-                                                                    Component.literal(
-                                                                            "You do not have permission to do that!")
-                                                                            .withStyle(ChatFormatting.RED));
-                                                }
-                                                return 1;
-                                            }))));
-                });
-    }
+  public static void register() {
+    ClientCommandRegistrationCallback.EVENT.register(
+        (dispatcher, registryAccess) -> {
+          dispatcher.register(ClientCommandManager.literal("sre:client")
+              .then(ClientCommandManager.literal("screen")
+                  .then(ClientCommandManager.literal("GameManagePanel")
+                      .executes(context -> {
+                        if (context.getSource().getPlayer().hasPermissions(2)) {
+                          ClientScheduler.schedule(() -> {
+                            context.getSource().getClient()
+                                .setScreen(new GameManagementScreen());
+                          }, 1);
+                        } else {
+                          context.getSource()
+                              .sendError(
+                                  Component.literal(
+                                      "You do not have permission to do that!")
+                                      .withStyle(ChatFormatting.RED));
+                        }
+                        return 1;
+                      }))
+                  .then(ClientCommandManager.literal("config")
+                      .executes(context -> {
+                        ClientScheduler.schedule(() -> {
+                          context.getSource().getClient()
+                              .setScreen(new SettingMenuScreen(null));
+                        }, 1);
+                        return 1;
+                      }))));
+        });
+  }
 }
