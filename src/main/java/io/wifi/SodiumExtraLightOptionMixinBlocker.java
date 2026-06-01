7c86ed868c9c868252bbfa51ce474108660e6877
wifi-left <ysy1679682327@outlook.com>
2026-06-01 21:05:45 +0800

    强制光照更新

--- src/main/java/io/wifi/SodiumExtraLightOptionMixinBlocker.java
+++ src/main/java/io/wifi/SodiumExtraLightOptionMixinBlocker.java
@@ -9,8 +9,15 @@ import java.util.List;
 public class SodiumExtraLightOptionMixinBlocker implements MixinCanceller {
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
-        if (mixinClassName.equals("me.flashyreese.mods.sodiumextra.mixin.light_updates.MixinLevelLightEngine")) {
-            SRE.LOGGER.info("Blocked sodium-extra mixin: [MixinLevelLightEngine]");
+        List<String> bannedMixins = List.of(
+                "me.flashyreese.mods.sodiumextra.mixin.light_updates.MixinLevelLightEngine", // 光照
+                "me.flashyreese.mods.sodiumextra.mixin.render.entity.MixinItemFrameEntityRenderer", // 物品展示框
+                "me.flashyreese.mods.sodiumextra.mixin.render.entity.MixinLivingEntityRenderer", // 盔甲架
+                "me.flashyreese.mods.sodiumextra.mixin.render.entity.MixinPaintingEntityRenderer" // 画
+        );
+        // ItemFrameRenderer
+        if (bannedMixins.contains(mixinClassName)) {
+            SRE.LOGGER.info("Blocked sodium-extra mixin: [" + mixinClassName + "]");
             return true;
         }
         return false;
