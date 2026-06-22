fab4b7814f31f6daf1b4df180955028ebb5f17bd
haiman <83174104+haiman233@users.noreply.github.com>
2026-06-23 00:27:52 +0800

    音效

--- src/main/java/org/agmas/noellesroles/init/NRSounds.java
+++ src/main/java/org/agmas/noellesroles/init/NRSounds.java
@@ -38,6 +38,7 @@ public class NRSounds {
     public static final SoundEvent SNOW_STORM = registrar.create("noellesroles.winter_storm");
     public static final SoundEvent SAND_STORM = registrar.create("noellesroles.dust_storm");
     public static final SoundEvent BROKEN_ALARM = registrar.create("noellesroles.broken_alarm");
+    public static final SoundEvent CIRCUS_BACKGROUND = registrar.create("noellesroles.circus_background");
 
     public static void initialize() {
         registrar.registerEntries();
