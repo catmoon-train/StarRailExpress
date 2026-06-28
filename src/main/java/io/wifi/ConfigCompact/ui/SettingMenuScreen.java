6026c145d2389315bb530a59eb1cfcab07d5c219
wifi-left <ysy1679682327@outlook.com>
2026-06-28 21:16:33 +0800

    True fix

--- src/main/java/io/wifi/ConfigCompact/ui/SettingMenuScreen.java
+++ src/main/java/io/wifi/ConfigCompact/ui/SettingMenuScreen.java
@@ -3,6 +3,9 @@ package io.wifi.ConfigCompact.ui;
 import io.wifi.starrailexpress.SREClientConfig;
 import io.wifi.starrailexpress.SREConfig;
 import net.minecraft.ChatFormatting;
+import net.minecraft.SharedConstants;
+import net.minecraft.client.InputType;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
@@ -10,8 +13,10 @@ import net.minecraft.client.gui.components.StringWidget;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.client.gui.screens.PauseScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 
 import java.util.function.Supplier;
@@ -19,11 +24,17 @@ import java.util.function.Supplier;
 import org.agmas.harpymodloader.config.HarpyModLoaderConfig;
 import org.agmas.noellesroles.client.screen.RoleIntroduceScreen;
 import org.agmas.noellesroles.config.NoellesRolesConfig;
+import org.slf4j.Logger;
+
+import com.mojang.blaze3d.vertex.BufferUploader;
+import com.mojang.logging.LogUtils;
+
 import pro.fazeclan.river.stupid_express.StupidExpressConfig;
 
 public class SettingMenuScreen extends Screen {
     Screen parent;
     boolean isFromPausingScreen = false;
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public SettingMenuScreen(Screen parent, boolean isFromPausingScreen) {
         this(parent);
@@ -185,9 +196,10 @@ public class SettingMenuScreen extends Screen {
         }
         if (isFromPausingScreen) {
             // 返回原版菜单
-            rowHelper.addChild(Button.builder(Component.translatable("screen.starrailexpress.settings.backpausing"), (button) -> {
-                this.minecraft.setScreen(new PauseScreen(true));
-            }).width(WIDTH_BUTTON_WIDTH).build(), COLUMN_COUNT);
+            rowHelper.addChild(
+                    Button.builder(Component.translatable("screen.starrailexpress.settings.backpausing"), (button) -> {
+                        setScreenIgnoreMixins(new PauseScreen(true));
+                    }).width(WIDTH_BUTTON_WIDTH).build(), COLUMN_COUNT);
         }
         // 返回
         rowHelper.addChild(Button.builder(Component.translatable("gui.back"), (button) -> {
@@ -197,5 +209,47 @@ public class SettingMenuScreen extends Screen {
         gridLayout.arrangeElements();
         FrameLayout.alignInRectangle(gridLayout, 0, 0, this.width, this.height, 0.5F, 0.25F);
         gridLayout.visitWidgets(this::addRenderableWidget);
+    }
+
+    private void setScreenIgnoreMixins(Screen screen) {
+        Minecraft client = this.minecraft;
+        if (SharedConstants.IS_RUNNING_IN_IDE) {
+            LOGGER.error("setScreen called from non-game thread");
+        }
+
+        if (client.screen != null) {
+            client.screen.removed();
+        } else {
+            client.setLastInputType(InputType.NONE);
+        }
+        {
+            if (screen == null && client.level == null) {
+                screen = new TitleScreen();
+            } else if (screen == null && client.player.isDeadOrDying()) {
+                if (client.player.shouldShowDeathScreen()) {
+                    screen = new DeathScreen((Component) null, client.level.getLevelData().isHardcore());
+                } else {
+                    client.player.respawn();
+                }
+            }
+
+            client.screen = screen;
+            if (client.screen != null) {
+                client.screen.added();
+            }
+
+            BufferUploader.reset();
+            if (screen != null) {
+                client.mouseHandler.releaseMouse();
+                KeyMapping.releaseAll();
+                screen.init(client, client.getWindow().getGuiScaledWidth(), client.getWindow().getGuiScaledHeight());
+                client.noRender = false;
+            } else {
+                client.getSoundManager().resume();
+                client.mouseHandler.grabMouse();
+            }
+
+            client.updateTitle();
+        }
     }
 }
