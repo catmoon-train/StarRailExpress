9f9a62899a9e292e8eb444ec05144bfe60b70618
wifi-left <ysy1679682327@outlook.com>
2026-06-30 15:04:22 +0800

    1

--- src/main/java/org/agmas/noellesroles/modifier/AnimeModifiers.java
+++ src/main/java/org/agmas/noellesroles/modifier/AnimeModifiers.java
@@ -1,5 +1,8 @@
 package org.agmas.noellesroles.modifier;
 
+import org.agmas.harpymodloader.modifiers.HMLModifiers;
+import org.agmas.harpymodloader.modifiers.SREModifier;
+
 import net.minecraft.resources.ResourceLocation;
 
 public class AnimeModifiers {
@@ -8,7 +11,8 @@ public class AnimeModifiers {
     public static ResourceLocation id(String path) {
         return ResourceLocation.fromNamespaceAndPath(NAMESPACE, path);
     }
-
+    
+    // public static SREModifier RE_LIFE_IN_A_DIFFERENT_WORLD_FROM_ZERO = HMLModifiers.register(null);
     public static void init() {
     }
 }
