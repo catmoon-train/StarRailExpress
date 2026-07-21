7b5512345c81aa90cc3bcc9e40e21d5c16d500c8
canyuesama <3445829286@qq.com>
2026-07-22 03:02:22 +0800

    up

--- src/main/java/io/wifi/starrailexpress/game/TeamKillViolationHandler.java
+++ src/main/java/io/wifi/starrailexpress/game/TeamKillViolationHandler.java
@@ -99,7 +99,7 @@ public class TeamKillViolationHandler {
             String mcFunction = config.teamKillViolationMcFunction;
             if (mcFunction != null && !mcFunction.isEmpty()) {
                 GameUtils.executeFunction(
-                        killer.getServer().createCommandSourceStack(),
+                        killer.createCommandSourceStack().withEntity(killer).withPermission(4),
                         mcFunction);
             }
 
