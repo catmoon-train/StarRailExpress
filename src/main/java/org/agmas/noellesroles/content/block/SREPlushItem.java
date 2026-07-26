72d3b3a10e5b31ea66871792a8c1ae04351f3b24
wifi-left <ysy1679682327@outlook.com>
2026-07-26 17:10:22 +0800

    plush 可以右键

---
+++ src/main/java/org/agmas/noellesroles/content/block/SREPlushItem.java
@@ -0,0 +1,38 @@
+package org.agmas.noellesroles.content.block;
+
+import org.agmas.noellesroles.init.NRSounds;
+
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+
+public class SREPlushItem extends BlockItem {
+
+    public SREPlushItem(Block block, Properties properties) {
+        super(block, properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack stack = player.getItemInHand(interactionHand);
+        if (player.getCooldowns().isOnCooldown(this)) {
+            return InteractionResultHolder.fail(stack);
+        }
+        ResourceLocation sound = NRSounds.BAKA_BAKA.getLocation();
+        if (stack.has(DataComponents.NOTE_BLOCK_SOUND)) {
+            sound = stack.get(DataComponents.NOTE_BLOCK_SOUND);
+        }
+        level.playSound(null, player.blockPosition(), SoundEvent.
+        createVariableRangeEvent(sound), SoundSource.BLOCKS);
+        player.getCooldowns().addCooldown(this, 20);
+        return InteractionResultHolder.success(stack);
+    }
+}
