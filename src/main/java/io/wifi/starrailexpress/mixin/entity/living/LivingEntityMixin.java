2b0782af2d1893740b3cb8d9ad192af399ec9b64
wifi-left <ysy1679682327@outlook.com>
2026-07-28 15:08:30 +0800

    狼人能否在水下淹死的areaSettings

--- src/main/java/io/wifi/starrailexpress/mixin/entity/living/LivingEntityMixin.java
+++ src/main/java/io/wifi/starrailexpress/mixin/entity/living/LivingEntityMixin.java
@@ -1,6 +1,8 @@
 package io.wifi.starrailexpress.mixin.entity.living;
 
 import io.wifi.starrailexpress.SRE;
+import io.wifi.starrailexpress.cca.AreasWorldComponent;
+import io.wifi.starrailexpress.cca.SREGameWorldComponent;
 import io.wifi.starrailexpress.index.TMMItems;
 import io.wifi.starrailexpress.mixin.entity.EntityMixin;
 import net.minecraft.core.Holder;
@@ -18,11 +20,13 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends EntityMixin {
     @Unique
-    private static final AttributeModifier KNIFE_KNOCKBACK_MODIFIER = new AttributeModifier(SRE.id("knife_knockback_modifier"), 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
+    private static final AttributeModifier KNIFE_KNOCKBACK_MODIFIER = new AttributeModifier(
+            SRE.id("knife_knockback_modifier"), 1, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
 
     @Shadow
     protected boolean jumping;
@@ -33,11 +37,24 @@ public abstract class LivingEntityMixin extends EntityMixin {
     @Shadow
     public abstract @Nullable AttributeInstance getAttribute(Holder<Attribute> attribute);
 
+    @Inject(method = "decreaseAirSupply", at = @At("HEAD"), cancellable = true)
+    private void sre$decreaseAirSupply(int currentAir, CallbackInfoReturnable<Integer> cir) {
+        if ((Object) this instanceof Player player) {
+            if (SREGameWorldComponent.isKillerTeamStatic(player)) {
+                if (AreasWorldComponent.KEY.get(player.level()).areasSettings.killerNoDrowing) {
+                    cir.setReturnValue(currentAir);
+                }
+            }
+        }
+    }
+
     @Inject(method = "tick", at = @At("HEAD"))
     public void tmm$addKnockbackWithKnife(CallbackInfo ci) {
         if ((Object) this instanceof Player player) {
-            AttributeModifier v = new AttributeModifier(SRE.id("knife_knockback_modifier"), .5f, AttributeModifier.Operation.ADD_VALUE);
-            updateAttribute(player.getAttribute(Attributes.ATTACK_KNOCKBACK), v, player.getMainHandItem().is(TMMItems.KNIFE));
+            AttributeModifier v = new AttributeModifier(SRE.id("knife_knockback_modifier"), .5f,
+                    AttributeModifier.Operation.ADD_VALUE);
+            updateAttribute(player.getAttribute(Attributes.ATTACK_KNOCKBACK), v,
+                    player.getMainHandItem().is(TMMItems.KNIFE));
         }
     }
 
