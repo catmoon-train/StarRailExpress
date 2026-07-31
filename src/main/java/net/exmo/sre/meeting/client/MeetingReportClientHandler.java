725dbd82786227a782e21c2788f2152524a98a4e
wifi-left <ysy1679682327@outlook.com>
2026-07-31 20:50:57 +0800

    1

--- src/main/java/net/exmo/sre/meeting/client/MeetingReportClientHandler.java
+++ src/main/java/net/exmo/sre/meeting/client/MeetingReportClientHandler.java
@@ -4,6 +4,8 @@ import io.wifi.starrailexpress.cca.AreasWorldComponent;
 import io.wifi.starrailexpress.cca.SREGameTimeComponent;
 import io.wifi.starrailexpress.cca.SREGameWorldComponent;
 import io.wifi.starrailexpress.content.entity.PlayerBodyEntity;
+import io.wifi.starrailexpress.event.client.OnRenderRoleName;
+import io.wifi.utils.client.betterrender.FakeGuiGraphics;
 import net.exmo.sre.meeting.MeetingManager;
 import net.exmo.sre.meeting.network.MeetingCooldownS2CPayload;
 import net.fabricmc.api.EnvType;
@@ -11,11 +13,11 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.Font;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.EntityHitResult;
 import java.util.Set;
 import java.util.UUID;
@@ -51,14 +53,15 @@ public final class MeetingReportClientHandler {
             reportedBodies = Set.of();
         });
         ClientTickEvents.END_CLIENT_TICK.register(MeetingReportClientHandler::tick);
-        HudRenderCallback.EVENT.register(MeetingReportClientHandler::renderHint);
+        OnRenderRoleName.RENDER_END.register(MeetingReportClientHandler::renderHint);
     }
 
     private static void tick(Minecraft client) {
         // 上报逻辑已迁移到 MeetingClientHandler.tick() 中由分号键统一处理
     }
 
-    private static void renderHint(GuiGraphics g, DeltaTracker deltaTracker) {
+    private static void renderHint(Player player, float range, FakeGuiGraphics g,
+                DeltaTracker tickCounter, Font renderer) {
         Minecraft client = Minecraft.getInstance();
         if (client.options.hideGui || !canPrompt(client)) {
             return;
