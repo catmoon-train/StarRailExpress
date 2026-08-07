10a3a328e59aa410fdc8656c3e7a4cd3d0d3888d
wifi-left <ysy1679682327@outlook.com>
2026-08-07 18:18:59 +0800

    1

--- src/main/java/org/agmas/noellesroles/init/InitModRolesMax.java
+++ src/main/java/org/agmas/noellesroles/init/InitModRolesMax.java
@@ -375,10 +375,11 @@ public class InitModRolesMax {
             }
 
             {
-                // 杀手中立（只处理没有配置的职业：无概率 且 无显式 setMax）
+                // 杀手中立（只处理没有配置的职业：无概率 且 无显式 setMax 且 允许修改生成配置的职业）
                 var neutralRoles = new ArrayList<SRERole>(TMMRoles.ROLES.values());
                 neutralRoles.removeIf((r) -> {
                     if (r.isNeutrals() && r.isNeutralForKiller() && (r.spawnInfo.enableChance < 0)
+                            && r.canSetSpawnInfoInConfig()
                             && r.spawnInfo.maxSpawn < 0)
                         return false;
                     return true;
