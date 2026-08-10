6328216995c10339c267299f912f2238ef96563c
wifi-left <ysy1679682327@outlook.com>
2026-08-10 20:59:30 +0800

    检测非玻璃卡住

--- src/main/java/io/wifi/starrailexpress/client/gui/RoundTextRenderer.java
+++ src/main/java/io/wifi/starrailexpress/client/gui/RoundTextRenderer.java
@@ -456,16 +456,20 @@ public class RoundTextRenderer {
         int vigilanteWidth = getOrCacheWidth(renderer, vigilanteTitle);
         int killerWidth = getOrCacheWidth(renderer, killerTitle);
 
+        int sideTitleXOffset = (18 + winCenterColumn * 12);
+        
         int neutralY = (looseEndsTotal > winSideColumn - 1) ? (14 + 16 + 32 * ((looseEndsTotal) / 2)) : 14;
-        context.drawString(renderer, neutralTitle, -neutralWidth / 2 - (48 + winCenterColumn * 6) - 6 * winSideColumn,
+        context.drawString(renderer, neutralTitle, -neutralWidth / 2 - (sideTitleXOffset) - 6 * winSideColumn,
                 neutralY, 0xffffff);
         if (looseEndsTotal > winSideColumn - 1) {
-            context.drawString(renderer, looseEndRole, -looseEndWidth / 2 -  (48 + winCenterColumn * 6) - 6 * winSideColumn, 14,
+            context.drawString(renderer, looseEndRole,
+                    -looseEndWidth / 2 - (sideTitleXOffset) - 6 * winSideColumn, 14,
                     0xffffff);
         }
         context.drawString(renderer, civilianTitle, -civilianWidth / 2, 14, 0xFFFFFF);
-        context.drawString(renderer, vigilanteTitle, -vigilanteWidth / 2 + (48 + winCenterColumn * 6) + 6 * winSideColumn, 14, 0xFFFFFF);
-        context.drawString(renderer, killerTitle, -killerWidth / 2 +(48 + winCenterColumn * 6) + 6 * winSideColumn,
+        context.drawString(renderer, vigilanteTitle,
+                -vigilanteWidth / 2 + (sideTitleXOffset) + 6 * winSideColumn, 14, 0xFFFFFF);
+        context.drawString(renderer, killerTitle, -killerWidth / 2 + (sideTitleXOffset) + 6 * winSideColumn,
                 14 + 16 + 32 * ((vigilanteTotal) / 2),
                 0xFFFFFF);
     }
--- src/main/java/org/agmas/noellesroles/utils/StuckHelperUtils.java
+++ src/main/java/org/agmas/noellesroles/utils/StuckHelperUtils.java
@@ -1,20 +1,12 @@
 package org.agmas.noellesroles.utils;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SlabBlock;
-import net.minecraft.world.level.block.StairBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class StuckHelperUtils {
 
-    public static final double volumeThreshold = 0.01;
+    public static final double volumeThreshold = 0.03;
 
     public static boolean isPlayerStuck(Player player) {
         if (player.getVehicle() != null)
@@ -24,62 +16,6 @@ public class StuckHelperUtils {
         Level level = player.level();
         AABB playerBox = player.getBoundingBox();
 
-        int minX = Mth.floor(playerBox.minX);
-        int maxX = Mth.ceil(playerBox.maxX);
-        int minY = Mth.floor(playerBox.minY);
-        int maxY = Mth.ceil(playerBox.maxY);
-        int minZ = Mth.floor(playerBox.minZ);
-        int maxZ = Mth.ceil(playerBox.maxZ);
-
-        BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
-        for (int x = minX; x < maxX; x++) {
-            for (int y = minY; y < maxY; y++) {
-                for (int z = minZ; z < maxZ; z++) {
-                    pos.set(x, y, z);
-                    BlockState state = level.getBlockState(pos);
-                    if (canBlockStuckPlayer(level, pos, state)) {
-                        VoxelShape shape = state.getCollisionShape(level, pos);
-                        if (shape.isEmpty())
-                            continue;
-                        double totalOverlapVolume = 0.0;
-                        for (AABB subBox : shape.toAabbs()) {
-                            AABB movedBox = subBox.move(pos);
-                            AABB overlap = playerBox.intersect(movedBox);
-                            if (overlap != null) {
-                                totalOverlapVolume += overlap.getXsize() * overlap.getYsize() * overlap.getZsize();
-                                // 提前退出优化：一旦超过阈值即可判定卡住
-                                if (totalOverlapVolume > volumeThreshold) {
-                                    return true;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private static boolean canBlockStuckPlayer(Level level, BlockPos pos, BlockState state) {
-        if (state.isAir())
-            return false;
-        if (!state.getFluidState().isEmpty())
-            return false;
-        if (state.is(Blocks.POWDER_SNOW))
-            return false;
-
-        // 完整立方体碰撞箱 (石头、泥土、木板等) -> 卡人
-        if (state.isCollisionShapeFullBlock(level, pos))
-            return true;
-
-        // 白名单：楼梯和台阶，只要碰撞形状非空就纳入检测
-        Block block = state.getBlock();
-        if (block instanceof StairBlock || block instanceof SlabBlock) {
-            VoxelShape shape = state.getCollisionShape(level, pos);
-            return !shape.isEmpty();
-        }
-
-        // 门、活板门、栅栏、梯子等均被排除
-        return false;
+        return level.collidesWithSuffocatingBlock(player, playerBox);
     }
 }
