6e4330d6f5773e9610a6aa31e0d919709b00051c
wifi-left <ysy1679682327@outlook.com>
2026-08-20 01:08:56 +0800

    小问题?

--- src/main/java/org/agmas/noellesroles/client/NoellesrolesClient.java
+++ src/main/java/org/agmas/noellesroles/client/NoellesrolesClient.java
@@ -1746,7 +1746,6 @@ public class NoellesrolesClient implements ClientModInitializer {
     public static void clearTimeStopCache() {
         Minecraft.getInstance().execute(() -> {
             clientPositions.clear();
-            TimeStopEffect.clientCanMovePlayers.clear();
             TimeStopEffect.freezeStatedTime = 0;
             TimeStopEffect.freezeMaxTime = 0;
         });
