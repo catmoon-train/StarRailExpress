de5d5e37be6dbcba9e1b66c82cbec6ad35334e5f
wifi-left <ysy1679682327@outlook.com>
2026-08-21 19:53:50 +0800

    1

--- src/main/java/org/agmas/noellesroles/mixin/roles/host/MoneyIncreaseMixin.java
+++ src/main/java/org/agmas/noellesroles/mixin/roles/host/MoneyIncreaseMixin.java
@@ -55,11 +55,10 @@ public class MoneyIncreaseMixin {
                 SREPlayerPsychoComponent component = (SREPlayerPsychoComponent) SREPlayerPsychoComponent.KEY
                         .get(victim);
                 if (component.getPsychoTicks() <= 0) {
-                    component.startPsycho_time(GameConstants.getInTicks(0, 45), 1, true);
-                    component.armour = 0;
+                    component.startPsycho_time(GameConstants.getInTicks(0, 45), 0, true);
+                    SREGameWorldComponent.getInstance(victim).refreshPsychoCount(true);
                     ci.cancel();
                 }
-                SREGameWorldComponent.getInstance(victim).refreshPsychoCount(true);
             }
         }
     }
