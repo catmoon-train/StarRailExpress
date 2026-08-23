1f6e17ef30901c75e7de1759d6afe7a5a1380c51
wifi-left <ysy1679682327@outlook.com>
2026-08-23 19:56:10 +0800

    提高概率（不小心闪了个0）

--- src/main/java/org/agmas/noellesroles/role/ModRoles.java
+++ src/main/java/org/agmas/noellesroles/role/ModRoles.java
@@ -2340,7 +2340,7 @@ public class ModRoles {
                     InstinctType.custom(new Color(196, 148, 39).getRGB()))
             .setDefaultMax(1)
             .setDefaultEnableNeededPlayerCount(12)
-            .setDefaultEnableChance(500);
+            .setDefaultEnableChance(5000);
 
     /**
      * 魔术师角色 - 好人阵营（从模仿者移植）
