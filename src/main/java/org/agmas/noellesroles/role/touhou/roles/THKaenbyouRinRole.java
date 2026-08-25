7a4f968415d8fad3844072c69f640da9ca7a7261
wifi-left <ysy1679682327@outlook.com>
2026-08-25 17:17:40 +0800

    2

--- src/main/java/org/agmas/noellesroles/role/touhou/roles/THKaenbyouRinRole.java
+++ src/main/java/org/agmas/noellesroles/role/touhou/roles/THKaenbyouRinRole.java
@@ -38,7 +38,7 @@ public class THKaenbyouRinRole extends TouhouRole {
     public List<ShopEntry> getShopEntries(@Nullable Player player) {
         ArrayList<ShopEntry> SHOP = new ArrayList<>();
         SHOP.add(new KaenbyouRinShopEntry(TMMItems.CROWBAR.getDefaultInstance(), 3, ShopEntry.Type.TOOL));
-        SHOP.add(new KaenbyouRinShopEntry(TMMItems.REVOLVER.getDefaultInstance(), 8, ShopEntry.Type.TOOL));
+        SHOP.add(new KaenbyouRinShopEntry(TMMItems.REVOLVER.getDefaultInstance(), 6, ShopEntry.Type.TOOL));
         SHOP.add(new KaenbyouRinShopEntry(FunnyItems.SHISIYE.getDefaultInstance(), 10, ShopEntry.Type.TOOL));
         SHOP.add(new KaenbyouRinShopEntry(TMMItems.KNIFE.getDefaultInstance(), 12, ShopEntry.Type.TOOL));
         SHOP.add(new KaenbyouRinShopEntry(TMMItems.DEFENSE_VIAL.getDefaultInstance(), 14, ShopEntry.Type.TOOL));
