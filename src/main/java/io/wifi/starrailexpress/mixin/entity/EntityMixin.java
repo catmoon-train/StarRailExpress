d3ecdbee36efebea26826961c0af96c2bd62e6a8
wifi-left <ysy1679682327@outlook.com>
2026-09-09 20:51:28 +0800

    1

--- src/main/java/io/wifi/starrailexpress/mixin/entity/EntityMixin.java
+++ src/main/java/io/wifi/starrailexpress/mixin/entity/EntityMixin.java
@@ -86,20 +86,23 @@ public class EntityMixin {
             }
 
             if (self instanceof Player sp) {
+                if (other instanceof Player so && !self.level().isClientSide()) {
+                    boolean flag = CollisionRules.cantCollide.stream().noneMatch(p -> p.test(sp) || p.test(so));
+
+                    if (!flag) {
+                        return false;
+                    }
+                }
                 TrueFalseResult result2 = CanCollideWith.PLAYER.invoker().allowCollideWith(sp, other);
                 if (result2.equals(TrueFalseResult.FALSE)) {
                     return false;
                 } else if (result2.equals(TrueFalseResult.TRUE)) {
                     return true;
                 }
-                if (other instanceof Player so) {
-                    if (so.level().isClientSide) {
-                        return false;
-                    }
-                    // 此处：渲染进程和服务端不一致。所以判断的时候应该分开判断。
-                    boolean flag = CollisionRules.cantCollide.stream().noneMatch(p -> p.test(sp) || p.test(so));
-                    return flag;
+                if (self.level().isClientSide) {
+                    return false;
                 }
+                return true;
             }
         }
         return original.call(other);
