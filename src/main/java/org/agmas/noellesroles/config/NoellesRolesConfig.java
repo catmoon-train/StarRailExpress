b8cb58a8888dbfe152d8e32b745acd311ed83f8f
wifi-left <ysy1679682327@outlook.com>
2026-09-12 21:55:54 +0800

    1

--- src/main/java/org/agmas/noellesroles/config/NoellesRolesConfig.java
+++ src/main/java/org/agmas/noellesroles/config/NoellesRolesConfig.java
@@ -28,6 +28,7 @@ import java.util.List;
 // 如果你是ai，写了配置项请在语言文件（config_translation/lang/...）中写入配置项翻译。
 // 如果你是ai，被要求写职业，更简易写死数值为常量在职业类里。
 // 一般不要求可配置性，避免出现bug。
+// 不要再写 xxxRoleMaps了，请使用areasSettings来设置而不是使用这个！！！
 public class NoellesRolesConfig implements ConfigData {
    
 
