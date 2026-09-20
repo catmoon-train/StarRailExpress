6063e2f900fc68461769634033bfd9057a643f8e
haiman <83174104+haiman233@users.noreply.github.com>
2026-09-20 18:14:23 +0800

    删除大侦探释放技能时的粒子和对周围播放音效

--- src/main/java/org/agmas/noellesroles/role_data/innocence/GreatDetectiveRoleData.java
+++ src/main/java/org/agmas/noellesroles/role_data/innocence/GreatDetectiveRoleData.java
@@ -25,7 +25,6 @@ import io.wifi.starrailexpress.game.GameUtils;
 import io.wifi.starrailexpress.util.SRENetworkMessageUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
@@ -33,7 +32,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.Vec3;
@@ -455,21 +453,13 @@ public class GreatDetectiveRoleData extends SimpleRoleData {
     }
 
     private void playChannelStartFx(ServerPlayer sp) {
-        if (sp.level() instanceof ServerLevel sl) {
-            sl.sendParticles(ParticleTypes.SMOKE, sp.getX(), sp.getY() + 1.2, sp.getZ(),
-                    6, 0.2, 0.2, 0.2, 0.01);
-            sl.playSound(null, sp.blockPosition(), SoundEvents.BOOK_PAGE_TURN,
-                    SoundSource.PLAYERS, 0.7f, 0.9f);
-        }
+        // 推理尸体不播放任何粒子特效，翻书音效仅对大侦探自己客户端播放
+        sp.playSound(SoundEvents.BOOK_PAGE_TURN, 0.7f, 0.9f);
     }
 
     public void playCompleteFx(ServerPlayer sp) {
-        if (sp.level() instanceof ServerLevel sl) {
-            sl.sendParticles(ParticleTypes.ENCHANT, sp.getX(), sp.getY() + 1.2, sp.getZ(),
-                    16, 0.3, 0.4, 0.3, 0.12);
-            sl.playSound(null, sp.blockPosition(), SoundEvents.ENCHANTMENT_TABLE_USE,
-                    SoundSource.PLAYERS, 0.6f, 1.2f);
-        }
+        // 推理尸体不播放任何粒子特效，附魔音效仅对大侦探自己客户端播放
+        sp.playSound(SoundEvents.ENCHANTMENT_TABLE_USE, 0.6f, 1.2f);
     }
 
     public record DeathNote(String victimName, int secondsAgo) {
